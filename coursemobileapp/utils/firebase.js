aa18fa347446b00f444bbe6e41dba2a6ee3abc25
Cozg <nguyenhuucong295@gmail.com>
2026-01-03 12:00:55 +0700

    fix api

--- coursemobileapp/utils/firebase.js
+++ coursemobileapp/utils/firebase.js
@@ -7,15 +7,15 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyBTtT65rG3kj4fkBcqTVeGqQxdwJDmYilE",
-    authDomain: "courseapp-ffa5d.firebaseapp.com",
-    projectId: "courseapp-ffa5d",
-    storageBucket: "courseapp-ffa5d.firebasestorage.app",
-    messagingSenderId: "101409565422",
-    appId: "1:101409565422:web:28356915274053d82a21ff",
-    measurementId: "G-SWCYWD46BS"
+    apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+//const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
 const analytics = getAnalytics(app);
